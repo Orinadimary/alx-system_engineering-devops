689f2b568d8a5b24dbf90e114a7e6887fd8c9e89
Orinadimary <dimaryorina@gmail.com>
2024-01-22 10:23:40 -0800

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# create a file in /tmp.
+
+file { 'school':
+  ensure  => 'file',
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
